03172b301aca9aa5e22782ff9e8502b455ab9f1f
Sarthak Goel <goelsarthak09@gmail.com>
2023-07-31 12:20:03 +0530

    Changing response object in views

--- books/views.py
+++ books/views.py
@@ -6,32 +6,46 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-@api_view(['GET','POST'])
-def book_list(request, format= None):
-    if request.method=='GET':
-        books=Book.objects.all()
-        serializer= BookSerializer(books, many=True)
+@api_view(['GET', 'POST'])
+def book_list(request, format=None):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
-    if request.method=='POST':
-        serializer=BookSerializer(data=request.data)
+    if request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-@api_view(['GET','PUT','Delete'])
+
+
+@api_view(['GET', 'PUT', 'Delete'])
 def book_detail(request, id, format=None):
+
     try:
-        book=Book.objects.get(pk=id)
+        book = Book.objects.get(pk=id)
     except Book.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method=='GET':
-        serializer=BookSerializer(book)
-        return Response(serializer.data)
-    elif request.method=='PUT':
-        serializer=BookSerializer(book, data=request.data)
+        fail_data = {
+            "status": "failed",
+            "code": 404,
+            "message": "Book not found",
+            "data": {}
+        }
+        return Response(fail_data, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        response_data = {
+            "status": "success",
+            "code": 200,
+            "message": "User data retrieved successfully",
+            "data": serializer.data
+        }
+        return Response(response_data)
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-    elif request.method =='DELETE':
+    elif request.method == 'DELETE':
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
